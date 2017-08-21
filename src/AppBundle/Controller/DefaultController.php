c794c70a67f0c3b7c4bee8f894b19806197aa550
tankosic.branislav <tankosic.branislav@BCLWS082.nsinfo.local>
2017-08-21 12:58:15 +0200

    proba

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,16 +6,27 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
-{
-    /**
-     * @Route("/", name="homepage")
-     */
-    public function indexAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
-    }
+class DefaultController extends Controller {
+
+  /**
+   * @Route("/", name="homepage")
+   */
+  public function indexAction(Request $request) {
+    // replace this example code with whatever you need
+    return $this->render('default/index.html.twig', [
+                //'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+                'base_dir' => realpath($this->getParameter('kernel.root_dir') . '/..') . DIRECTORY_SEPARATOR,
+    ]);
+  }
+
+  /*
+   * @Route("/hello/{name}", name="hello_world")
+   */
+
+  public function greetingsAction(Request $request) {
+    $name = $request->get('name');
+    $twigParameters = ['greetings' => 'Hi! Hello', 'name' => $name];
+    return $this->render('AppBundle:default:index.html.twig', $twigParameters);
+  }
+
 }
---
+++ src/AppBundle/Controller/GenusController.php
@@ -0,0 +1,80 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace AppBundle\Controller;
+
+/**
+ * Description of GenusController
+ *
+ * @author tankosic.branislav
+ */
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+USE Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+class GenusController extends Controller{
+  
+  /**
+    *  @Route("/genus/{genusName}")
+    */
+  public function showAction($genusName) {
+    
+   
+    /* Sklonjam $notes odavde u jednom momentu kada zelim da naprvaim funkciju getNotesAction
+     * koja mi sluzi da prikazem podatke iz varijeble $notes. Tu punim podatke
+     * koji su lepse organizovani u json struktura
+    $notes = [
+            'Octopus asked me a riddle, outsmarted me',
+            'I counted 8 legs... as they wrapped around me',
+            'Inked!'
+    ];
+     * 
+     */
+    
+//    $templating = $this->container->get('templating');
+//    $html = $templating->render('genus/show.html.twig', array(
+//            'name' => $genusName
+//    ));
+//    
+//    return new Response($html);
+    
+    
+    
+    return $this->render('genus/show.html.twig', array(
+            'name' => $genusName,
+           //'notes' => $notes
+    ));
+    
+    
+  }
+  
+  /**
+   * 
+   * @Route("/genus/{genusName}/notes")
+   * @Method("GET")
+   */
+  public function getNotesAction() {
+   
+    $notes = [
+            ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Octopus asked me a riddle, outsmarted me', 'date' => 'Dec. 10, 2015'],
+            ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Dec. 1, 2015'],
+            ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Inked!', 'date' => 'Aug. 20, 2015'],
+    ];
+    
+    $data = [
+        'notes' => $notes,
+    ];
+            
+    //return new Response(json_encode($data));
+    return new 
+    
+  }
+  
+  
+}
